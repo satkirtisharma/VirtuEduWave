77e770727bbc875945a12c9548cd5dc493140198
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2023-11-30 09:19:04 +0530

    Add files via upload

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { config } from 'dotenv';
+config();
+
+const { MONGODB_URL } = process.env;
+
+export function connect() {
+	mongoose
+		.connect(MONGODB_URL, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
+		.catch((err) => {
+			console.log(`DB Connection Failed`);
+			console.log(err);
+			process.exit(1);
+		});
+};
